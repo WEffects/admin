01a709e4dda363849b3fd2e5af4040651f8c8bfe
Yogesh Singh <yogesh.singh@corider.in>
2024-08-30 19:21:25 +0530

    search feat added and entery action

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [modalImage, setModalImage] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
   // const baseUrl="http://localhost:5000/"
   const baseUrl="https://event-r4pe.onrender.com/"
 
@@ -62,15 +63,61 @@ const App = () => {
       return updatedData;
     });
   };
-
+  const handleEntered = async (ticketCode,index) => {
+    console.log("ticket",ticketCode)
+    try {
+      const response = await fetch(`${baseUrl}enter/${ticketCode}`, {
+        method: "PUT",
+        
+      });
+      if (!response.ok) {
+        throw new Error("Failed to confirm");
+      }
+      alert("Entry Confirmed!");
+      
+      updateEntryStatus(index)
+    } catch (error) {
+      alert(`Error: ${error.message}`);
+    }
+  };
+  const updateEntryStatus = (index) => {
+    setData((prevData) => {
+      const updatedData = { ...prevData };
+      updatedData.registered[index] = {
+        ...updatedData.registered[index],
+        entered: true,
+      };
+      return updatedData;
+    });
+  };
+  const filteredData = searchQuery
+    ? data.registered.filter((item) =>{
+        if(item.serialNumber){
+           return item.serialNumber
+           .toLowerCase()
+           .includes(searchQuery.toLowerCase())
+        }
+       
+      })
+    : data.registered;
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
 
   return (
-    <div className="p-4 m-4 min-w-[900px]">
+    <div className="p-4 m-4 min-w-[1100px]">
+
       <h1 className="text-2xl font-bold mb-4 text-center bg-black text-white p-2">Admin Panel</h1>
+      <div className="mb-4">
+        <input
+          type="text"
+          placeholder="Search by Ticket Code"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="border p-2 w-full"
+        />
+      </div>
       {
-        data.registered.length==0?<h1 className="text-2xl font-bold mb-4 text-center p-2">No Data Found</h1>
+        filteredData.length==0?<h1 className="text-2xl font-bold mb-4 text-center p-2">No Data Found</h1>
         : <table className="min-w-full bg-white mt-8">
         <thead>
           <tr>
@@ -78,42 +125,49 @@ const App = () => {
             <th className="py-2">Name</th>
             <th className="py-2">Email</th>
             <th className="py-2">Phone</th>
+            <th className="py-2">Ticket Code</th>
             <th className="py-2">Ticket Type</th>
             <th className="py-2">Total Tickets</th>
-            <th className="py-2">Attendance</th>
             <th className="py-2">Total Price</th>
             <th className="py-2">Registered At</th>
             <th className="py-2">Actions</th>
           </tr>
         </thead>
         <tbody>
-          {data.registered.map((item,index) => (
+          {filteredData.map((item,index) => (
             <tr key={item.id} className="border text-center">
               <td className="py-2 border">{index+1}</td>
               <td className="py-2 border ">{item.name[0]}</td>
               <td className="py-2 border">{item.email}</td>
               <td className="py-2 border">{item.phone}</td>
+              <td className="py-2 border">{item.serialNumber?item.serialNumber:"--"}</td>
               <td className="py-2 border">{item.ticketType}</td>
               <td className="py-2 border">{item.totalTickets}</td>
-              <td className="py-2 border">{item.attendence}</td>
               <td className="py-2 border">₹ {item.totalPrice}</td>
               <td className="py-2 border">
                
                 {new Date(item.registerAt).toLocaleDateString()}
               </td>
-              <td className="py-2 ">
+              <td className="py-2 flex justify-center ">
                 <button
-                  className="bg-blue-500 text-white px-2 py-1 rounded m-1 mr-2 "
+                  className="bg-blue-500 text-white rounded p-1 m-1"
                   onClick={() => handleShowScreenshot(item.ticketImageUrl)} // Add screenshotUrl to your data
                 >
                   Show Screenshot
                 </button>
                 <button
-                  className={`${item.confirm ? "bg-gray-500" : "bg-green-500"} text-white px-2 py-1 rounded`}
+                  className={` p-1 m-1 ${item.confirm ? "bg-gray-500" : "bg-green-500 px-3"} text-white rounded`}
                   onClick={() => handleConfirm(item.ticketCode,index)}
                   disabled={item.confirm}
                 >
-                  Confirm
+                 {item.confirm ? "Confirmed" : "Confirm"}
+                </button>
+                <button
+                  className={` p-1 m-1 ${item.entered ? "bg-gray-500" : "bg-green-500 px-3"} text-white rounded`}
+                  onClick={() => handleEntered(item.ticketCode,index)}
+                  disabled={item.entered}
+                >
+                 {item.entered ? "Entered" : "Enter"}
                 </button>
               </td>
             </tr>
